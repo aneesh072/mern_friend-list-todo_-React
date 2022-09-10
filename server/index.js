3106ee0913e43dfa300019a5c6f29ef995ea57cc
aneesh072 <aneeshkarki292@gmail.com>
2022-09-10 21:01:10 +1000

    connecting mongo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('moongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const FriendModel = require('./models/Friends');
 
 //database connection
+mongoose.connect(
+  'mongodb+srv://admin:admin@cluster0.rafogxh.mongodb.net/merntutorial?retryWrites=true&w=majority'
+);
 
-app.listen(3001, ()=>{
-    console.log('You are connected');
-})
+app.get('/insert', async (req, res) => {
+  const friend = new FriendModel({ name: 'wqerfwer', age: 24 });
+  await friend.save();
+  res.send('Inserted data');
+});
+
+app.get('/read', async (req, res) => {
+  FriendModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3002, () => {
+  console.log('You are connected');
+});
